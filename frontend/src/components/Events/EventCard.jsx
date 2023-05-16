49b571fb84a0e930c30318bc7a3e07a906a612df
Quốc Dũng <91933007+QuocDung21@users.noreply.github.com>
2023-05-16 10:37:08 +0700

    success

--- frontend/src/components/Events/EventCard.jsx
+++ frontend/src/components/Events/EventCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { backend_url } from "../../server";
 import styles from "../../styles/styles";
 import CountDown from "./CountDown";
@@ -10,7 +10,7 @@ import { toast } from "react-toastify";
 const EventCard = ({ active, data }) => {
   const { cart } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
-
+  console.log("data", data);
   const addToCartHandler = (data) => {
     const isItemExists = cart && cart.find((i) => i._id === data._id);
     if (isItemExists) {
@@ -24,7 +24,7 @@ const EventCard = ({ active, data }) => {
         toast.success("Item added to cart successfully!");
       }
     }
-  }
+  };
   return (
     <div
       className={`w-full block bg-white rounded-lg ${
@@ -32,33 +32,56 @@ const EventCard = ({ active, data }) => {
       } lg:flex p-2`}
     >
       <div className="w-full lg:-w[50%] m-auto">
-        <img src={`${backend_url}${data.images[0]}`} alt="" />
+        <img src={`${backend_url}${data?.images[0]}`} alt="" />
       </div>
-      <div className="w-full lg:[w-50%] flex flex-col justify-center">
-        <h2 className={`${styles.productTitle}`}>{data.name}</h2>
-        <p>{data.description}</p>
-        <div className="flex py-2 justify-between">
-          <div className="flex">
-            <h5 className="font-[500] text-[18px] text-[#d55b45] pr-3 line-through">
-              {data.originalPrice}$
-            </h5>
-            <h5 className="font-bold text-[20px] text-[#333] font-Roboto">
-              {data.discountPrice}$
-            </h5>
+      {data != undefined ? (
+        <>
+          {" "}
+          <div className="w-full lg:[w-50%] flex flex-col justify-center">
+            <h2 className={`${styles.productTitle}`}>{data?.name}</h2>
+            <p>{data?.description}</p>
+            <div className="flex py-2 justify-between">
+              <div className="flex">
+                <h5 className="font-[500] text-[18px] text-[#d55b45] pr-3 line-through">
+                  {data?.originalPrice}$
+                </h5>
+                <h5 className="font-bold text-[20px] text-[#333] font-Roboto">
+                  {data?.discountPrice}$
+                </h5>
+              </div>
+              <span className="pr-3 font-[400] text-[17px] text-[#44a55e]">
+                {data?.sold_out} sold
+              </span>
+            </div>
+            <CountDown data={data} />
+            <br />
+            <div className="flex items-center">
+              <Link to={`/product/${data?._id}?isEvent=true`}>
+                <div className={`${styles.button} text-[#fff]`}>
+                  See Details
+                </div>
+              </Link>
+              <div
+                className={`${styles.button} text-[#fff] ml-5`}
+                onClick={() => addToCartHandler(data)}
+              >
+                Add to cart
+              </div>
+            </div>
           </div>
-          <span className="pr-3 font-[400] text-[17px] text-[#44a55e]">
-            {data.sold_out} sold
-          </span>
-        </div>
-        <CountDown data={data} />
-        <br />
-        <div className="flex items-center">
-          <Link to={`/product/${data._id}?isEvent=true`}>
-            <div className={`${styles.button} text-[#fff]`}>See Details</div>
-          </Link>
-          <div className={`${styles.button} text-[#fff] ml-5`} onClick={() => addToCartHandler(data)}>Add to cart</div>
-        </div>
-      </div>
+        </>
+      ) : (
+        <>
+          <div className="w-full lg:[w-50%] flex flex-col justify-center">
+            <div className="flex py-2 ">
+              <div className="flex">
+                <h1 className="text-2xl">No products Found!</h1>
+              </div>
+            </div>
+            <br />
+          </div>
+        </>
+      )}
     </div>
   );
 };
